2c9f3397ffb3fa7836ea08a0a858c39b308f2383
Blain Smith <rebelgeek@blainsmith.com>
2016-03-02 12:58:38 -0500

    copy and catalog

--- javascripts/custom-repos.js
+++ javascripts/custom-repos.js
@@ -1,6 +1,7 @@
 // Opt-in repos (case sensitive)
 // - These are all children of Odd Networks and visible at https://github.com/Shopify
 var optInRepos = [
+  'seneca-odd-catalog',
   'seneca-odd-auth',
   'seneca-odd-views',
   'odd-schemas',
@@ -15,19 +16,10 @@ var optInRepos = [
 var customRepos = []
 
 // Custom repo language, different than that defined by GitHub
-var customRepoLanguage = {
-  'liquid': 'Liquid',
-  'Timber': 'Liquid',
-  'skeleton-theme': 'Liquid',
-  'dashing': 'Ruby',
-  'shopify_theme': 'Ruby',
-  'Shopify-Developer-Book': 'Ruby',
-  'offsite-gateway-sim': 'Ruby',
-  'shopify.github.com': 'JavaScript',
-  'batman': 'JavaScript'
-}
+var customRepoLanguage = {}
 
 var customRepoCategories = {
+  'seneca-odd-catalog': 'Platform',
   'seneca-odd-auth': 'Platform',
   'seneca-odd-views': 'Platform',
   'odd-schemas': 'Platform',
@@ -39,9 +31,4 @@ var customRepoCategories = {
 
 // Custom repo avatars. Dimensions should be 40x40
 // - Be sure a custom repo doesn't have the same name as a Shopify one, or a one will be overridden
-var customRepoAvatar = {
-  'Timber': '/images/repo-avatars/timber.gif',
-  'dashing': '/images/repo-avatars/dashing.gif',
-  'superdb': '/images/repo-avatars/super-debugger.gif',
-  'batman': '/images/repo-avatars/batmanjs.gif'
-}
+var customRepoAvatar = {}
